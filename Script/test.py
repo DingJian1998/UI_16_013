d32b4c287bc3ed5d62d915a62ca7d6294de659c9
benben <1941679069@qq.com>
2020-08-08 14:29:28 +0800

    添加

--- Script/test.py
+++
@@ -1,4 +0,0 @@
-class Test:
-    def test_001(self):
-        print('\ntest_001')
-        assert True
--- Script/test_01.py
+++ Script/test_01.py
@@ -1,4 +1,11 @@
+import allure
+
 class Test_01:
+    @allure.step('1')
     def test_01(self):
         print('\ntest_01')
-        assert False
+        assert True
+    @allure.step('2')
+    def test_02(self):
+        print('\ntest_02')
+        assert True
